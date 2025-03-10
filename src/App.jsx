ad620845d8f0755e9a39880dad67fd3040e5f468
FazleRifat121 <fazlerifatofficial@gmail.com>
2025-03-10 20:57:24 +0600

    new rep created

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import Search from "./component/search/Search";
+
+function App() {
+  const [searchTerm, setSearchTerm] = useState("");
+  return (
+    <main>
+      <div className="pattern" />
+      <div className="wrapper">
+        <header>
+          <img src="hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You Will Enjoy
+            Without the Hassle
+          </h1>
+          <h1 className="text-white">{searchTerm}</h1>
+        </header>
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm}></Search>
+      </div>
+    </main>
+  );
+}
+
+export default App;
